e61337f7d16d63d3cfad00ad573b35dffdad6612
Yuji Kamiya <y.kamiya0@gmail.com>
2021-01-30 12:48:00 +0900

    custom classification head

--- trainer.py
+++ trainer.py
@@ -108,13 +108,45 @@ class SemEval2018EmotionDataset(Dataset):
         return len(self.texts)
 
 
+class CustomClassificationHead(nn.Module):
+    def __init__(self, config, input_dim):
+        super().__init__()
+        self.config = config
+
+        self.fc1 = nn.Linear(input_dim, 4096)
+        self.fc2 = nn.Linear(4096, 2048)
+        self.fc3 = nn.Linear(2048, 1024)
+        self.fc4 = nn.Linear(1024, config.n_labels)
+        self.dropout = nn.Dropout(p=0.3)
+        self.prelu1 = nn.PReLU()
+        self.prelu2 = nn.PReLU()
+        self.prelu3 = nn.PReLU()
+
+        nn.init.kaiming_normal_(self.fc1.weight)
+        nn.init.kaiming_normal_(self.fc2.weight)
+        nn.init.kaiming_normal_(self.fc3.weight)
+        nn.init.kaiming_normal_(self.fc4.weight)
+
+    def forward(self, x):
+        # dropout is applied before this method is called
+        # https://github.com/huggingface/transformers/blob/v4.1.1/src/transformers/models/bert/modeling_bert.py#L1380
+        x = self.prelu1(self.fc1(x))
+        x = self.prelu2(self.fc2(self.dropout(x)))
+        x = self.prelu3(self.fc3(self.dropout(x)))
+        return self.fc4(self.dropout(x))
+
+
 class Trainer:
     def __init__(self, config):
         self.config = config
 
         model_name = 'cl-tohoku/bert-base-japanese-whole-word-masking' if config.lang == 'ja' else 'bert-base-uncased'
         self.tokenizer = BertTokenizer.from_pretrained(model_name, padding=True)
-        self.model = BertForSequenceClassification.from_pretrained(model_name, num_labels=config.n_labels, return_dict=True).to(config.device)
+        self.model = BertForSequenceClassification.from_pretrained(model_name, num_labels=config.n_labels, return_dict=True)
+        if config.custom_head:
+            self.model.classifier = CustomClassificationHead(config, self.model.config.hidden_size)
+        self.model.to(config.device)
+
         self.optimizer = AdamW(self.model.parameters(), lr=config.lr)
         self.warmup_scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lambda step: min(1.0, (step + 1) / config.warmup_steps))
 
@@ -332,6 +364,7 @@ if __name__ == '__main__':
     parser.add_argument('--warmup_steps', type=int, default=1000)
     parser.add_argument('--multi_labels', action='store_true')
     parser.add_argument('--dataset_class_name', default='EmotionDataset', choices=['EmotionDataset', 'SemEval2018EmotionDataset'])
+    parser.add_argument('--custom_head', action='store_true')
     args = parser.parse_args()
 
     pd.options.display.precision = 3
